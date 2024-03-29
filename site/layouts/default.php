28814df46b1093eced37b238c7e5574976bb9dd4
Jan Heise <janheise@mail.de>
2024-03-29 01:16:35 +0100

    Add SEO Snippet to Layout

--- site/layouts/default.php
+++ site/layouts/default.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php snippet(name: 'meta_information') ?>
+    <?php snippet(name: 'robots') ?>
     <?= css(url: 'assets/css/app.css') ?>
 </head>
 
